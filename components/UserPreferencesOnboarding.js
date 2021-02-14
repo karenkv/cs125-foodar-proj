a5a2edbbd862df806ec0c2b992303bc7691291a8
Rebecca Leung <rjleung@uci.edu>
2021-02-13 16:10:59 -0800

    Changing swip card styles and connecting to Home screen with navigation

--- components/UserPreferencesOnboarding.js
+++ components/UserPreferencesOnboarding.js
@@ -25,7 +25,11 @@ class NoMoreCards extends Component {
   render(){
     return (
       <View>
-        <Text style={styles.NoMoreCards}>That's all!</Text>
+        <Text style={styles.NoMoreCards}>Thanks for letting us know!</Text>
+        <Button
+          title="Done"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
       </View>
     )
   }
@@ -34,14 +38,17 @@ class NoMoreCards extends Component {
 export default class UserPreferencesOnboarding extends Component {
   constructor(props) {
     super(props);
+    bgColor = "white";
+    const foodPrefOptions = [
+      { text: "Dairy", backgroundColor: bgColor },
+      { text: "Low-Carb", backgroundColor: bgColor },
+      { text: "Seafood", backgroundColor: bgColor },
+      { text: "Red Meat", backgroundColor: bgColor },
+      { text: "Vegetarian", backgroundColor: bgColor },
+      { text: "Vegan", backgroundColor: bgColor },
+    ];
     this.state = {
-      cards: [
-        {text: 'Dairy', backgroundColor: 'yellow'},
-        {text: 'Seafood', backgroundColor: 'blue'},
-        {text: 'Red Meat', backgroundColor: 'green'},
-        {text: 'Vegetarian', backgroundColor: 'purple'},
-        {text: 'Vegan', backgroundColor: 'purple'},
-      ]
+      cards: foodPrefOptions
     };
   }
 
@@ -57,29 +64,83 @@ export default class UserPreferencesOnboarding extends Component {
   
   render() {
     return (
-      <SwipeCards
-        cards={this.state.cards}
-        renderCard={(cardData) => <Card {...cardData} />}
-        keyExtractor={(cardData) => String(cardData.text)}
-        renderNoMoreCards={() => <NoMoreCards />}
-        stack={true}
-        stackDepth={3}
-        handleYup={this.handleYup}
-        handleNope={this.handleNope}
-      />
-    )
+      <View style={styles.container}>
+        <Text style={styles.title}>Eating habits</Text>
+        <SwipeCards
+          cards={this.state.cards}
+          renderCard={(cardData) => <Card {...cardData} />}
+          keyExtractor={(cardData) => String(cardData.text)}
+          renderNoMoreCards={() => <NoMoreCards {...this.props} />}
+          stack={true}
+          stackDepth={3}
+          stackOffsetX={10}
+          showYup={true}
+          showNope={true}
+          handleYup={this.handleYup}
+          handleNope={this.handleNope}
+          yupText={"Like ❤️"}
+          nopeText={"Dislike 💔"}
+          yupStyle={styles.yup}
+          nopeStyle={styles.nope}
+          yupTextStyle={styles.yupText}
+          nopeTextStyle={styles.nopeText}
+        />
+    </View>
+    );
   }
 }
 
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#F8AD9D",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "#f8f8ff",
+    fontSize: 32,
+    alignContent: "center",
+    alignSelf: "center",
+    top: 120,
+    position: "relative",
+  },
   card: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
     width: 300,
-    height: 300,
+    height: 400,
+  },
+  cardsText: {
+    fontSize: 22,
+    color: "black",
   },
   noMoreCardsText: {
-    fontSize: 22,
-  }
+    fontSize: 46,
+  },
+  yup: {
+    borderColor: "transparent",
+    position: "absolute",
+    padding: 20,
+    bottom: 50,
+    right: 0,
+  },
+  yupText: {
+    fontSize: 18,
+    color: "#f8f8ff",
+  },
+  nope: {
+    borderColor: "transparent",
+    position: "absolute",
+    padding: 20,
+    bottom: 50,
+    left: 0,
+  },
+  nopeText: {
+    fontSize: 18,
+    color: "#f8f8ff",
+  },
 });
