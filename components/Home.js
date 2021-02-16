4e7d13ab8a079f3a2ea7f0e9d2b41ace6197d882
Karen Vu <karenkv@uci.edu>
2021-02-16 00:37:10 -0800

    added icons to assets

--- components/Home.js
+++ components/Home.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, FlatList } from 'react-native';
 
-export default class Login extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Home</Text>
-      </View>
-    )
-  }
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            icons: [
+                require('../assets/home.png'),
+                require('../assets/search.png'),
+                require('../assets/profile.png'),
+            ]
+        };
+      }
+
+    render() {
+        return (
+            <>
+            <View>
+                <Text style={styles.header}>home</Text>
+            </View>
+            <View>
+                <Image source={require('../assets/divider.png')}/>
+            </View>
+            <View>
+                <FlatList
+                    horizontal={true} 
+                    showsHorizontalScrollIndicator={false} 
+                    data={this.state.icons}
+                    renderItem={({item}) => (<Image source={item}/>)}
+                    keyExtractor={(index) => index.toString()}
+                />
+            </View>
+            </>
+        )
+    }
 }
 
 
 const styles = StyleSheet.create({
-
+    header: {
+        color: '#D22624',
+        fontSize: 72,
+        fontWeight: 'bold',
+    }
 });
