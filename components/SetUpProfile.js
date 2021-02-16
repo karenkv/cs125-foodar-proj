db6be55e003279e54b58b6a4d0915200d41576c4
Rebecca Leung <rjleung@uci.edu>
2021-02-16 14:23:02 -0800

    Adding SetUpProfile file

---
+++ components/SetUpProfile.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+
+export default class Profile extends Component {
+    render() {
+      return (
+        <SafeAreaView styles={styles.container}>
+            
+        </SafeAreaView>
+      )
+    }
+  }
+  
+  
+  const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+      },
+  });
