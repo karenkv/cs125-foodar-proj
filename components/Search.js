f5cb252fbf39b7ccda0d78fbf424c7e6a04ba29f
Rebecca Leung <rjleung@uci.edu>
2021-02-28 17:38:41 -0800

    Adding Yelp API to Search/Recommendation

--- components/Search.js
+++ components/Search.js
@@ -1,13 +1,87 @@
 import React, { Component } from 'react';
 import {View, Text, StyleSheet, Image, ScrollView, TextInput} from 'react-native';
 import Navigation from './Navigation';
+import axios from 'axios';
+import Config from 'react-native-config';
+
+const YELP_API_URL = "https://api.yelp.com/v3/businesses/search";
 
 export default class Search extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            isLoading: true,
+            origin: { latitude:33.650340, longitude: -117.839313 },
+            searchText: "",
         };
-      }
+
+        config = {
+            headers: { Authorization: `Bearer ${Config.YELP_API_KEY}`, },
+            params: {
+                term: 'healthy',
+                latitude: this.state.origin.latitude,
+                longitude: this.state.origin.longitude,
+                limit: 10,
+            },
+        }
+    }
+    
+    getLocation = () => {
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    let newOrigin = {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                    };
+                    config.params.latitude = newOrigin.latitude;
+                    config.params.longitude = newOrigin.longitude;
+
+                    this.setState({
+                        origin: newOrigin,
+                    });
+                    resolve(true);
+                },
+                err => {
+                    console.log('Error getting location');
+                    console.log(err);
+                    reject(reject);
+                },
+                { enableHighAccuracy: true, timeout: 2000, maximumAge: 1000 }
+            );
+        });
+    };
+
+    fetchMarkerData() {
+        console.log("Fetching data...");
+        return axios
+                .get(YELP_API_URL, config)
+                .then(responseJson => {
+                    this.setState({isLoading: false, markers: responseJson.data.businesses.map(x => x),});
+                })
+                .catch(error => {
+                    console.log('Error with making request')
+                    console.log(error);
+                });
+    }
+
+    changeSearchInput(inputText) {
+        this.setState({ searchText: inputText, });
+    }
+
+    async endEditing() {
+        const inputText = this.state.searchText;
+        if (inputText != "") {
+            console.log(inputText);
+            config.params.term = inputText; 
+            await this.fetchMarkerData();
+        }
+    }
+
+    async componentDidMount() {
+        //await this.getLocation();
+        await this.fetchMarkerData();
+    }
 
     render() {
         return (
@@ -20,9 +94,10 @@ export default class Search extends Component {
                     <View style={styles.searchContainer}>
                         <TextInput 
                             style={styles.searchInput} 
-                            keyboardType="numbers-and-punctuation"
                             placeholder="search 'sushi'"
                             placeholderTextColor={this.placeholderTextColor}
+                            onChangeText={text => this.changeSearchInput(text)}
+                            onEndEditing={() => this.endEditing()}
                         />
                         <Image style={styles.searchIcon} source={require('../assets/search.png')} />
                     </View>
@@ -90,5 +165,8 @@ const styles = StyleSheet.create({
         paddingLeft: 15,
         height: 50,
         borderColor: "transparent",
+    },
+    scrollView: {
+        marginHorizontal: 10,
     }
 });
